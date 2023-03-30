2aa2acf0e501c60a680dd239f59fe9c6c02de562
SkApsana1 <124445045+SkApsana1@users.noreply.github.com>
2023-03-30 16:25:13 +0530

    Update main.js

--- temperature1/main.js
+++ temperature1/main.js
@@ -1,1 +1,20 @@
-
+function converter()
+            {
+                var op=document.querySelector("#option");
+                var res=document.querySelector(".value");
+                var ip=document.querySelector(".t").value;
+                if(op.value=="fahrenheit")
+                {
+                    var c=(ip-32)*(5/9)
+                    res.innerHTML=c+"°C"
+                }
+                if(op.value=="celsius")
+                {
+                    var f=ip*9/5+32
+                    res.innerHTML=f+"°F"
+                }
+            }
+            function clearForm()
+            {
+                document.getElementById('degree').value='';
+            }
